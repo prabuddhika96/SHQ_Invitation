abb801d46f60a2091969c4daebbd7925820c158c
Prabuddhika Mayurapaksha <74924715+Prabuddhika96@users.noreply.github.com>
2022-09-27 09:32:44 +0530

    add 16 batch

--- index.js
+++ index.js
@@ -9,109 +9,164 @@ let name = '';
 
 if (encryptedname == 'N7K4mMufZQOGeAG') {
 	name = 'Prof. Amalka Pinidiyaarachchi';
-} else if (encryptedname == 'Y0O5FS89EEKPG') {
+}
+else if (encryptedname == 'Y0O5FS89EEKPG') {
 	name = 'Mr. Prabath Gunathilake';
-} else if (encryptedname == 'b0xKODsbmR') {
+} 
+else if (encryptedname == 'b0xKODsbmR') {
 	name = 'Dr. Erunika O. Dayarathne';
-} else if (encryptedname == 'Km5U2EM4FG2') {
+} 
+else if (encryptedname == 'Km5U2EM4FG2') {
 	name = 'Dr. Ruwan D. Nawarathne';
-} else if (encryptedname == '6A9kFJVdQw7dDlO') {
+} 
+else if (encryptedname == '6A9kFJVdQw7dDlO') {
 	name = 'Dr. Sachith P. Abeysundara';
-} else if (encryptedname == 'iaISZPlowy2L5Ahzy21') {
+} 
+else if (encryptedname == 'iaISZPlowy2L5Ahzy21') {
 	name = 'Dr. Hakim A. Usoof';
-} else if (encryptedname == 'nASNyp3Kjaa1v5Nm') {
+} 
+else if (encryptedname == 'nASNyp3Kjaa1v5Nm') {
 	name = 'Dr. S. A. Ruwan Punchi Manage';
-} else if (encryptedname == 'WGitCAgVUhRKmxLFR') {
+} 
+else if (encryptedname == 'WGitCAgVUhRKmxLFR') {
 	name = 'Dr. Hemalika T. K. Abeysundara';
-} else if (encryptedname == '0KVtwD7c7n2NU6') {
+} 
+else if (encryptedname == '0KVtwD7c7n2NU6') {
 	name = 'Dr. Lakshika S. Nawarathna';
-} else if (encryptedname == '0s4tW0czTuAfvmv3') {
+} 
+else if (encryptedname == '0s4tW0czTuAfvmv3') {
 	name = 'Dr. Ruwanthini Siyambalapitiya';
-} else if (encryptedname == 'Ki522Wa/n2CkyzL') {
+} 
+else if (encryptedname == 'Ki522Wa/n2CkyzL') {
 	name = 'Dr. Mahasen Dehideniya';
-} else if (encryptedname == 'Yvxuh4DWtlCih78') {
+} 
+else if (encryptedname == 'Yvxuh4DWtlCih78') {
 	name = 'Dr. Jagath Senarathne';
-} else if (encryptedname == 'CN5BxtA8hApoKiw') {
+} 
+else if (encryptedname == 'CN5BxtA8hApoKiw') {
 	name = 'Mr. M. A. P. Chamikara';
-} else if (encryptedname == '3HJwnpboOmCFtBC') {
+} 
+else if (encryptedname == '3HJwnpboOmCFtBC') {
 	name = 'Mr. Bhashithe Abeysinghe';
-} else if (encryptedname == 'pAhayQqND1dyfk') {
+} 
+else if (encryptedname == 'pAhayQqND1dyfk') {
 	name = 'Mrs. Sankhapali Polgolla';
-} else if (encryptedname == 'PmrNlTEclvzHqv4') {
+} 
+else if (encryptedname == 'PmrNlTEclvzHqv4') {
 	name = 'Mr. Pramuka Weerasinghe';
-} else if (encryptedname == '+4e/teSJcOJ/r1XF') {
+} 
+else if (encryptedname == '+4e/teSJcOJ/r1XF') {
 	name = 'Mr. Pavithra Basnayake';
-} else if (encryptedname == 'wZ21pjnhePGj2l') {
+} 
+else if (encryptedname == 'wZ21pjnhePGj2l') {
 	name = 'Mrs. Sathya Wijewardhana ';
-} else if (encryptedname == 'KfMlplJcuDL/Gwx') {
+} 
+else if (encryptedname == 'KfMlplJcuDL/Gwx') {
 	name = 'Dr. B. S. Dassanayake';
-} else if (encryptedname == '8xDvJTpUfUO3Pdu') {
+} 
+else if (encryptedname == '8xDvJTpUfUO3Pdu') {
 	name = 'Dr. U. B. Amarasinghe';
-} else if (encryptedname == 'p9Z0f8E+6ZWxMpc2') {
+} 
+else if (encryptedname == 'p9Z0f8E+6ZWxMpc2') {
 	name = 'Dr. T. H. K. R. De Silva';
-} else if (encryptedname == 'RWMwzTXbFCnp1W') {
+} 
+else if (encryptedname == 'RWMwzTXbFCnp1W') {
 	name = 'Dr. Chandima Dhanapala';
-} else if (encryptedname == 'MNacD/N4g4r0+5') {
+} 
+else if (encryptedname == 'MNacD/N4g4r0+5') {
 	name = 'Dr. V. Sivakumar';
-} else if (encryptedname == 'IAV9hLAoel+77') {
+} 
+else if (encryptedname == 'IAV9hLAoel+77') {
 	name = 'Mr. Mapa Wakkumbura';
-} else if (encryptedname == 'oA+2INk5Rr1Sr0p') {
+} 
+else if (encryptedname == 'oA+2INk5Rr1Sr0p') {
 	name = 'Mr. Priyantha Galgepitiya';
-} else if (encryptedname == '144xvpctHSwfmXd') {
+} 
+else if (encryptedname == '144xvpctHSwfmXd') {
 	name = 'Mr. Dinesh Deepal';
-} else if (encryptedname == 'HbpFm1EZBcITbm8') {
+} 
+else if (encryptedname == 'HbpFm1EZBcITbm8') {
 	name = 'Mrs. Anoma Perera';
-} else if (encryptedname == 'BllVmX47wNpYS') {
+} 
+else if (encryptedname == 'BllVmX47wNpYS') {
 	name = 'Mrs. Anusha Abeykoon';
-} else if (encryptedname == '7KmVrAhAZzdR6s1') {
+} 
+else if (encryptedname == '7KmVrAhAZzdR6s1') {
 	name = 'Mr. W. M. Ananda Bandara';
-} else if (encryptedname == 'WmgmudUSWE3hEn7') {
+} 
+else if (encryptedname == 'WmgmudUSWE3hEn7') {
 	name = 'Mr. Lasantha Senanayake';
-} else if (encryptedname == 'yZKPqBhuBUOYEbaIj') {
+} 
+else if (encryptedname == 'yZKPqBhuBUOYEbaIj') {
 	name = 'Mr. Chamara M. K. Herath';
-} else if (encryptedname == 'NuGfEILEPrhTyF') {
+} 
+else if (encryptedname == 'NuGfEILEPrhTyF') {
 	name = 'Ms. Himansi Kumari';
-} else if (encryptedname == 'ZmcGeh03B2g5cSS') {
+} 
+else if (encryptedname == 'ZmcGeh03B2g5cSS') {
 	name = 'Prof. Pushpakanthie Wijekoon';
-} else if (encryptedname == 'FoU6PTkvmoIVkX8I') {
+} 
+else if (encryptedname == 'FoU6PTkvmoIVkX8I') {
 	name = 'Prof. Saluka R. Kodithuwakku';
-} else if (encryptedname == 'b1hV7WDQ7wNCDzv') {
+} 
+else if (encryptedname == 'b1hV7WDQ7wNCDzv') {
 	name = 'Prof. Roshan D. Yapa';
-} else if (encryptedname == 'LSIuVbPN1Q6+50c/') {
+} 
+else if (encryptedname == 'LSIuVbPN1Q6+50c/') {
 	name = 'Prof. R. G. S. C. Rajapakse';
-} else if (encryptedname == '4AYV/8MFHV80hO') {
+} 
+else if (encryptedname == '4AYV/8MFHV80hO') {
 	name = 'Prof. L. R. A. K. Bandara';
-} else if (encryptedname == 'lhir954JhSptBLZ') {
+} 
+else if (encryptedname == 'lhir954JhSptBLZ') {
 	name = 'Prof. K. M. G. Gehan Jayasuriya';
-} else if (encryptedname == 'gy646Lcbs9kcepH+h') {
+} 
+else if (encryptedname == 'gy646Lcbs9kcepH+h') {
 	name = 'Prof. Manawadevi Y. U. Ganehenage';
-} else if (encryptedname == '10y9k+DZmICP6i2') {
+} 
+else if (encryptedname == '10y9k+DZmICP6i2') {
 	name = 'Prof. Inoka Karunaratne';
-} else if (encryptedname == '3GJ0H72iD+Lnty5') {
+} 
+else if (encryptedname == '3GJ0H72iD+Lnty5') {
 	name = 'Mr. Rochana Obadage';
-} else if (encryptedname == 'CvNlwHRQGnp3giro') {
+} 
+else if (encryptedname == 'CvNlwHRQGnp3giro') {
 	name = 'Mrs. Nethmee Gunasekara';
-} else if (encryptedname == 'VbhdoTr29FAJFkc') {
+} 
+else if (encryptedname == 'VbhdoTr29FAJFkc') {
 	name = 'Mr. Lasantha Senanayake';
-} else if (encryptedname == 'UmGU77mpkjdmNCwm8') {
+} 
+else if (encryptedname == 'UmGU77mpkjdmNCwm8') {
 	name = 'Mrs. Dilusha Senarathne';
-} else if (encryptedname == 'K7gKipa4yC2ARw2') {
+} 
+else if (encryptedname == 'K7gKipa4yC2ARw2') {
 	name = 'Mrs. Indeewari Baalasooriya';
-} else if (encryptedname == 'p3Qmjmib++42fs8T') {
+} 
+else if (encryptedname == 'p3Qmjmib++42fs8T') {
 	name = 'Mrs. Hasini Herath';
-} else if (encryptedname == 'bLrBEb/1IJGobPwT') {
+} 
+else if (encryptedname == 'bLrBEb/1IJGobPwT') {
 	name = 'Mrs. Buddhini Samarasinghe';
-} else if (encryptedname == '0tYf4i/tnZzx2Hve') {
+} 
+else if (encryptedname == '0tYf4i/tnZzx2Hve') {
 	name = 'Mr. Rajitha Edirisinghe';
-} else if (encryptedname == 'qsMxepHQLhLSJRY0') {
+} 
+else if (encryptedname == 'qsMxepHQLhLSJRY0') {
 	name = 'Mr. Samitha Herath';
-} else if (encryptedname == 'F7ddj/bu9Oz+oZu') {
+} 
+else if (encryptedname == 'F7ddj/bu9Oz+oZu') {
 	name = 'Mrs. Vindya Prabhawi';
-} else if (encryptedname == 'FIqk9kEZv8yzONN') {
+} 
+else if (encryptedname == 'FIqk9kEZv8yzONN') {
 	name = 'Mrs. Subrina Muthusara';
-} else if (encryptedname == 'Bpm6c8IvnV2oxnjlG') {
+} 
+else if (encryptedname == 'Bpm6c8IvnV2oxnjlG') {
 	name = 'non academic staff';
-} else {
+} 
+else if (encryptedname == 'gLKAqaOI30n8uV') {
+	name = '16\' Batch';
+}
+else {
 	name = 'you';
 }
 
